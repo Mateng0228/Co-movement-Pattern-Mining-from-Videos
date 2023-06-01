b87a8c3a8a01012ffa014617b5cc170878b51a8f
junnanhu <jnhu@zju.edu.cn>
2023-06-02 01:00:14 +0800

    change project structure

---
+++ AprioriBaseline/HeadFiles.h
@@ -0,0 +1,24 @@
+//
+// Created by lenovo on 2022/12/19.
+//
+
+#ifndef CONVOYBASELINE_HEADFILES_H
+#define CONVOYBASELINE_HEADFILES_H
+
+#endif //CONVOYBASELINE_HEADFILES_H
+
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <sys/io.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <map>
+#include <set>
+typedef long long ll;
+using namespace std;
---
+++ AprioriBaseline/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "Utils.h"
+#include "TestDemo.h"
+
+
+int main(int argc, char *argv[]) {
+    clock_t start_time, end_time;
+    start_time = clock();
+    sscanf(argv[1], "%lld", &m);
+    sscanf(argv[2], "%lld", &k);
+    sscanf(argv[3], "%lld", &eps);
+    string index = argv[4];
+
+    path = "../../../dataset/";
+    path += index;
+    //path = "../dataset/singapore";
+    cout<<"m = "<<m<<", k = "<<k<<", eps = "<<eps<<endl;
+    // path = "../dataset/video_sg/t14_c600_len16";
+    cout<<"Path = "<<path<<endl;
+    //getAllFiles(fileName, path, ".txt");
+    GetFileNames(path, fileName);
+    getData(objectRoute, fileName, objectId);
+
+
+    // 测试输出文件路径
+    // printFileName(fileName);
+
+    // @Test:输出每个车辆的路径信息
+    // printObjectRoute(objectRoute, objectId);
+
+    // comCamera : 每个摄像头下经过的所有的object集合，用pair表示，first是objectId， second用来表示startTime
+    // nextCamera : 每个摄像头可能经过的后一个摄像头
+    getCommonRoute(comCamera, nextCamera, objectRoute);
+
+    // @Test
+    // printCommonRoute(comCamera, nextCamera, objectId);
+
+    // comTimeRoute 目前为连续1个摄像头的所有满足m与exps的convoy模式
+    getComTimeIntervalRoute(comTimeRoute, m, eps);
+
+    // @Test
+    // printComTimeRoute(comTimeRoute, objectId);
+    // printNextCameraId();
+    // testPrintVectorPair();
+
+    // 获得convoy模式
+    getConvoyPattern(k, m);
+
+    // 最后一步对convoy进行去重
+    clock_t s_time, e_time;
+    s_time = clock();
+    dupConvoy(convoyPattern);
+    e_time = clock();
+    dupTime += (double) (e_time - s_time) / CLOCKS_PER_SEC;
+    // printComTimeRoute(comTimeRoute, objectId);
+    printComTimeRoute(convoyPattern, objectId);
+
+    end_time = clock();
+    printf("running time : %lf(s)\n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
+    printf("duplicating time : %lf(s)\n", dupTime);
+    printf("candidate number: %lld\n", candidate_num);
+
+
+    return 0;
+}
