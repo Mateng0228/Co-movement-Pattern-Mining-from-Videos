14d800d5372ae94672f0a10abaf099288b0e867d
guosongyuan <1294664581@qq.com>
2023-06-02 00:49:38 +0800

    change project structure

---
+++ CMCBaseline/CMC_beta.h
@@ -0,0 +1,16 @@
+//
+// Created by guosongyuan on 2023/5/15.
+//
+
+#ifndef CMC_CMC_BETA_H
+#define CMC_CMC_BETA_H
+
+#endif //CMC_CMC_BETA_H
+
+#include "utils.h"
+class CMC_beta{
+private:
+    
+public:
+
+};
